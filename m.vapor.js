9c50d2432204a31485f43c5981decc9713dfac92
westnorthern <rrichardson610@gmail.com>
2017-12-13 16:53:52 -0700

    sen

---
+++ m.vapor.js
@@ -0,0 +1,27 @@
+alert('The Assyrian came down like the wolf on the fold, \n And his cohorts were gleaming in purple and gold;\nAnd the sheen of their spears was like stars on the sea,
+When the blue wave rolls nightly on deep Galilee.
+
+   Like the leaves of the forest when Summer is green,
+That host with their banners at sunset were seen:
+Like the leaves of the forest when Autumn hath blown,
+That host on the morrow lay withered and strown.
+
+   For the Angel of Death spread his wings on the blast,
+And breathed in the face of the foe as he passed;
+And the eyes of the sleepers waxed deadly and chill,
+And their hearts but once heaved, and for ever grew still!
+
+   And there lay the steed with his nostril all wide,
+But through it there rolled not the breath of his pride;
+And the foam of his gasping lay white on the turf,
+And cold as the spray of the rock-beating surf.
+
+   And there lay the rider distorted and pale,
+With the dew on his brow, and the rust on his mail:
+And the tents were all silent, the banners alone,
+The lances unlifted, the trumpet unblown.
+
+   And the widows of Ashur are loud in their wail,
+And the idols are broke in the temple of Baal;
+And the might of the Gentile, unsmote by the sword,
+Hath melted like snow in the glance of the Lord!')
