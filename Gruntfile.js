699e63f65c50a876ac5c4824165996158f0a83e7
Cosas de Puma <kikefontanlorenzo@gmail.com>
2018-05-29 14:47:18 +0200

    Pull #79

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,12 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    babel: {
+      dist: {
+        src: ['vapor.js'],
+        dest: 'vapor.js'
+      }
+    },
     concat: {
       options: {
         separator: ';'
@@ -26,13 +32,14 @@ module.exports = function(grunt) {
     }
   });
 
+  grunt.loadNpmTasks('grunt-babel');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-qunit');
   grunt.loadNpmTasks('grunt-contrib-concat');
 
   grunt.registerTask('test', ['jshint', 'qunit']);
-  grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
-  grunt.registerTask('build', ['concat', 'uglify']);
+  grunt.registerTask('default', ['jshint', 'qunit', 'babel', 'concat', 'uglify']);
+  grunt.registerTask('build', ['babel', 'concat', 'uglify']);
 
 };
